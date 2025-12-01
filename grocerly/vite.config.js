1d8b4f5c2198d90cd3302223ae188d6d9f096f1f
jaaunt <jaaunt@taltech.ee>
2025-12-01 21:04:00 +0200

    config.js no dev tools for ci test 2

--- grocerly/vite.config.js
+++ grocerly/vite.config.js
@@ -2,15 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    // only enable devtools in development, not in CI
-    process.env.CI ? undefined : vueDevTools(),
-  ].filter(Boolean), // Remove undefined values
+    // Don't load devtools plugin in CI environment
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
